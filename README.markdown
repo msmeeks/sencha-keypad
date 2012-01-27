2c02adb2ceb76bb7951f22bf8fa4033b93ff676f
Michael <mmeeks@mmeeks-desktop.(none)>
2012-01-27 09:36:52 -0500

    First commit

---
+++ README.markdown
@@ -0,0 +1,25 @@
+# Sench Keypa: A Sencha Touch user extension
+
+Sencha Keypad was originally designed as a log in keypad for a mobile exam system called MobileLink at [BrightLink Technology](htp://thebrightlink.com).
+
+
+## Demo
+
+![Screenshot](http://aehlke.github.com/tag-it/screenshot.png)
+
+Check the [example.html](http://github.com/msmeeks/sencha-keypad/example.html) for several demos.
+
+
+## Usage
+Coming soon!
+
+This is the first item in my to-do list.
+## Authors
+
+* [Michael S. Meeks](http://github.com/msmeeks) *original author*
+
+
+## License
+
+sencha-keypad is released under the [MIT license](http://github.com/msmeeks/sencha-keypad/raw/master/LICENSE).
+
---
+++ js/Keypad.js
@@ -0,0 +1,157 @@
+Ext.namespace('Ext.ux');
+
+Ext.ux.Keypad = function(config) {
+	config = config || {};
+
+	config.xtype = 'keypad';
+	config.draggable = false;
+	config.scroll = false;
+	config.ui = config.ui || 'normal';
+
+	var width = 500;
+	var height = 650;
+	var button_ui = 'action';
+	var key_style = 'font-size:27pt;margin:5px;';
+	if (config.ui == 'small'){
+		width = 300;
+		height = 350;
+		button_ui = 'confirm-small';
+		key_style = 'font-size:20pt;margin:5px;';
+	}
+
+	this.createDigitKey = function(digit) {
+		return {
+			xtype: 'button',
+			text: digit,
+			handler: function(){
+				this.addDigit(digit);
+			}, scope: this
+		}
+	};
+
+	this.keypad_display = new Ext.form.Text({
+		id: 'keypad_value',
+		disabled: true,
+		value: ''
+	});
+
+	this.submit_button = new Ext.Button({
+		text: config.submit_text || 'Submit',
+		ui: button_ui,
+		handler: function() {
+			if (config.submit_url) {
+				var params = {};
+				params[config.submit_param_name || 'value'] = this.getValue();
+
+				Ext.Ajax.request({
+					url: config.submit_url,
+					params: params,
+					success: function(response, opts){
+						var data = Ext.util.JSON.decode(response.responseText);
+						if (data.success && data.redirect_url) {
+							window.location.href = data.redirect_url;
+						} else {
+							alert(data.msg);
+						}
+					},
+					failure: function(response, opts){
+						var data = Ext.util.JSON.decode(response.responseText);
+						var message = data.msg || 'There was an error submitting the input.';
+
+						Ext.ux.MessageBox.alert('Error',message);
+					},
+					scope: this
+				});
+			} else if (config.submit_handler) {
+				config.submit_handler(this.getValue());
+			} else {
+				Ext.Msg.alert('No submission action specified','value: ' + this.getValue());
+			}
+		},
+		scope: this
+	});
+
+	this.title_bar = {};
+	if (config.title){
+		this.title_bar = {
+			dock: 'top',
+			xtype: 'toolbar',
+			title: config.title
+		};
+	}
+
+	this.keypad_panel = new Ext.Panel({
+		title: 'Panel',
+		id: 'keypad_panel',
+		width:width,
+		height:height,
+		layout: {
+			type: 'vbox',
+			pack: 'center',
+			align: 'stretch'
+		},
+		defaults: {
+			layout: 'hbox',
+			align: 'stretch',
+			flex:1,
+			defaults: {
+				style:key_style,
+				flex:1
+			}
+		},
+		dockedItems: [this.title_bar],
+		items: [{
+			items: [this.keypad_display]
+		},{
+			items: [this.createDigitKey('1'), this.createDigitKey('2'), this.createDigitKey('3')]
+		}, {
+			items: [this.createDigitKey('4'), this.createDigitKey('5'), this.createDigitKey('6')]
+		}, {
+			items: [this.createDigitKey('7'), this.createDigitKey('8'), this.createDigitKey('9')]
+		}, {
+			items: [{},this.createDigitKey('0'),{},]
+		}, {
+			items: [{
+				xtype: 'button',
+				ui: button_ui,
+				text: 'Clear',
+				handler: function(){
+					this.clear()
+				},scope:this
+			}, 
+				this.submit_button
+			]
+		}]
+	});
+
+	if (config.centered){
+		config.layout = {type:'hbox', pack:'center'};
+	}
+	config.height = height;
+
+	Ext.apply(this, config);
+
+	Ext.ux.Keypad.superclass.constructor.call(this, config);
+	this.add( this.keypad_panel );
+	this.on('afterrender', function(){this.doLayout();},this);
+}
+
+Ext.extend(Ext.ux.Keypad, Ext.Panel, {
+	addDigit: function(digit) {
+		this.setValue(this.keypad_display.value + digit);
+	},
+
+	clear: function() {
+		this.keypad_display.setValue('');
+	},
+
+	setValue: function(value) {
+		this.keypad_display.setValue(value);
+	},
+
+	getValue: function() {
+		return this.keypad_display.getValue();
+	}
+});
+
+Ext.reg('keypad', Ext.ux.Keypad);
