7c46b87e31357b6a50c5fdac3ce36da10b3ba67b
Michael <mmeeks@mmeeks-laptop.(none)>
2012-12-15 14:26:44 -0500

    Actually committing the upgrade to Sencha Touch 2.1.0

--- js/Keypad.js
+++ js/Keypad.js
@@ -1,157 +1,164 @@
 Ext.namespace('Ext.ux');
 
-Ext.ux.Keypad = function(config) {
-	config = config || {};
+Ext.define('Ext.ux.Keypad', {
+    extend: 'Ext.Panel',
+    requires: [
+        'Ext.Panel',
+        'Ext.MessageBox',
+        'Ext.field.Text',
+        'Ext.Ajax',
+        'Ext.util.JSON'
+    ],
+    alias: 'widget.keypad',
 
-	config.xtype = 'keypad';
-	config.draggable = false;
-	config.scroll = false;
-	config.ui = config.ui || 'normal';
+    config: {
+        draggable: false,
+        scroll: false,
+        ui: 'normal',
+        centered: false,
+        clearText: 'Clear',
+        submitText: 'Submit',
+        submitUrl: '',
+        submitParamName: 'value',
+        submitHandler: null
+    },
 
-	var width = 500;
-	var height = 650;
-	var button_ui = 'action';
-	var key_style = 'font-size:27pt;margin:5px;';
-	if (config.ui == 'small'){
-		width = 300;
-		height = 350;
-		button_ui = 'confirm-small';
-		key_style = 'font-size:20pt;margin:5px;';
-	}
+    constructor: function(config) {
+        config = config || {};
 
-	this.createDigitKey = function(digit) {
-		return {
-			xtype: 'button',
-			text: digit,
-			handler: function(){
-				this.addDigit(digit);
-			}, scope: this
-		}
-	};
+        if (config.centered){
+            config.layout = {type:'hbox', pack:'center'};
+        }
+        this.callParent([config]);
 
-	this.keypad_display = new Ext.form.Text({
-		id: 'keypad_value',
-		disabled: true,
-		value: ''
-	});
+        var width = 500;
+        var height = 650;
+        var button_ui = 'action';
+        var key_style = 'font-size:27pt;margin:5px;';
+        if (this.getUi() == 'small'){
+            width = 300;
+            height = 350;
+            button_ui = 'confirm-small';
+            key_style = 'font-size:20pt;margin:5px;';
+        }
 
-	this.submit_button = new Ext.Button({
-		text: config.submitText || 'Submit',
-		ui: button_ui,
-		handler: function(b, e) {
-			if (config.submitUrl) {
-				var params = {};
-				params[config.submitParamName || 'value'] = this.getValue();
+        this.keypadDisplay = Ext.create('Ext.field.Text', {
+            disabled: true,
+            value: ''
+        });
 
-				Ext.Ajax.request({
-					url: config.submitUrl,
-					params: params,
-					success: function(response, opts){
-						var data = Ext.util.JSON.decode(response.responseText);
-						if (data.success && data.redirect_url) {
-							window.location.href = data.redirect_url;
-						} else {
-							alert(data.msg);
-						}
-					},
-					failure: function(response, opts){
-						var data = Ext.util.JSON.decode(response.responseText);
-						var message = data.msg || 'There was an error submitting the input.';
+        this.clearButton = Ext.create('Ext.Button', {
+            ui: button_ui,
+            text: this.getClearText(),
+            handler: function(){
+                this.clear()
+            },
+            scope: this
+        });
 
-						Ext.ux.MessageBox.alert('Error',message);
-					},
-					scope: this
-				});
-			} else if (config.submitHandler) {
-				config.submitHandler(this.getValue(), this, b, e);
-			} else {
-				Ext.Msg.alert('No submission action specified','value: ' + this.getValue());
-			}
-		},
-		scope: this
-	});
+        this.submitButton = Ext.create('Ext.Button', {
+            text: this.getSubmitText(),
+            ui: button_ui,
+            handler: function(b, e) {
+                if (this.getSubmitUrl()) {
+                    var params = {};
+                    params[this.getSubmitParamName()] = this.getValue();
 
-	this.title_bar = {};
-	if (config.title){
-		this.title_bar = {
-			dock: 'top',
-			xtype: 'toolbar',
-			title: config.title
-		};
-	}
+                    Ext.Ajax.request({
+                        url: this.getSubmitUrl(),
+                        params: params,
+                        success: function(response, opts){
+                            var data = Ext.util.JSON.decode(response.responseText);
+                            if (data.success && data.redirect_url) {
+                                window.location.href = data.redirect_url;
+                            } else {
+                                var message = data.msg || 'There was an error submitting the input.';
 
-	this.keypad_panel = new Ext.Panel({
-		title: 'Panel',
-		id: 'keypad_panel',
-		width:width,
-		height:height,
-		layout: {
-			type: 'vbox',
-			pack: 'center',
-			align: 'stretch'
-		},
-		defaults: {
-			layout: 'hbox',
-			align: 'stretch',
-			flex:1,
-			defaults: {
-				style:key_style,
-				flex:1
-			}
-		},
-		dockedItems: [this.title_bar],
-		items: [{
-			items: [this.keypad_display]
-		},{
-			items: [this.createDigitKey('1'), this.createDigitKey('2'), this.createDigitKey('3')]
-		}, {
-			items: [this.createDigitKey('4'), this.createDigitKey('5'), this.createDigitKey('6')]
-		}, {
-			items: [this.createDigitKey('7'), this.createDigitKey('8'), this.createDigitKey('9')]
-		}, {
-			items: [{},this.createDigitKey('0'),{},]
-		}, {
-			items: [{
-				xtype: 'button',
-				ui: button_ui,
-				text: config.clearText || 'Clear',
-				handler: function(){
-					this.clear()
-				},scope:this
-			}, 
-				this.submit_button
-			]
-		}]
-	});
+                                //Ext.Msg.alert('Error', message);
+                                alert(message);
+                            }
+                        },
+                        failure: function(response, opts){
+                            var data = Ext.util.JSON.decode(response.responseText);
+                            var message = data.msg || 'There was an error submitting the input.';
 
-	if (config.centered){
-		config.layout = {type:'hbox', pack:'center'};
-	}
-	config.height = height;
+                            //Ext.Msg.alert('Error', message);
+                            alert(message);
+                        },
+                        scope: this
+                    });
+                } else if (this.getSubmitHandler()) {
+                    this.getSubmitHandler()(this.getValue(), this, b, e);
+                } else {
+                    //Ext.Msg.alert('No submission action specified', 'Value: ' + this.getValue());
+                    alert('Value: ' + this.getValue());
+                }
+            },
+            scope: this
+        });
 
-	Ext.apply(this, config);
+        this.createDigitKey = function(digit) {
+            return {
+                xtype: 'button',
+                text: digit,
+                handler: function(){
+                    this.addDigit(digit);
+                }, scope: this
+            }
+        };
 
-	Ext.ux.Keypad.superclass.constructor.call(this, config);
-	this.add( this.keypad_panel );
-	this.on('afterrender', function(){this.doLayout();},this);
-}
 
-Ext.extend(Ext.ux.Keypad, Ext.Panel, {
+        this.keypadPanel = Ext.create('Ext.Panel', {
+            width: width,
+            height: height,
+            layout: {
+                type: 'vbox',
+                pack: 'center',
+                align: 'stretch'
+            },
+            defaults: {
+                layout: 'hbox',
+                align: 'stretch',
+                flex: 1,
+                defaults: {
+                    style: key_style,
+                    flex: 1
+                }
+            },
+            items: [{
+                items: [this.keypadDisplay]
+            },{
+                items: [this.createDigitKey('1'), this.createDigitKey('2'), this.createDigitKey('3')]
+            }, {
+                items: [this.createDigitKey('4'), this.createDigitKey('5'), this.createDigitKey('6')]
+            }, {
+                items: [this.createDigitKey('7'), this.createDigitKey('8'), this.createDigitKey('9')]
+            }, {
+                items: [{},this.createDigitKey('0'),{}]
+            }, {
+                items: [
+                    this.clearButton, 
+                    this.submitButton
+                ]
+            }]
+        });
+
+        this.add(this.keypadPanel);
+    },
+
 	addDigit: function(digit) {
-		this.setValue(this.keypad_display.value + digit);
+		this.setValue(this.getValue() + digit);
 	},
 
 	clear: function() {
-		this.keypad_display.setValue('');
+		this.keypadDisplay.setValue('');
 	},
 
 	setValue: function(value) {
-		this.keypad_display.setValue(value);
+		this.keypadDisplay.setValue(value);
 	},
 
 	getValue: function() {
-		return this.keypad_display.getValue();
+		return this.keypadDisplay.getValue();
 	}
 });
-
-Ext.reg('keypad', Ext.ux.Keypad);
